476aa42552c99bcee4c880e5230a24de259769ef
Joshua Leven <josh@noredink.com>
2016-09-24 19:39:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rise::Application.config.secret_key_base = 'e4628d362b48ccdb0959abe005c26565548c0e8e3e356a51c43bf998eeef9bfd136b594a8b71ec5074bdddcd8ed6429d3a06794cdad5413edb1ca949770b58cd'
